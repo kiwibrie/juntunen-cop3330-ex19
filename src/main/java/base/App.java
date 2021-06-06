580c7ba9af91a80e7359c066298a3e8e60097055
kiwibrie <brianne0128@gmail.com>
2021-06-06 15:51:47 -0500

    working base

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -11,5 +11,40 @@ public class App {
 
     public static void main(String[] args) {
         App myApp = new App();
+
+        int weight = myApp.promptInt("weight in pounds");
+        int height = myApp.promptInt("height in inches");
+
+        double bmi = myApp.calculateBMI(weight, height);
+
+        myApp.printOutput(bmi);
+    }
+
+    public void printOutput(double bmi){
+        System.out.printf("Your BMI is %.1f.\n", bmi);
+        System.out.print(generateResponse(bmi));
+    }
+
+    public String generateResponse(double bmi){
+        if(bmi < 18.5){
+            return "You are underweight. You should see your doctor.";
+        } else if (bmi <= 25){
+            return "You are within the ideal weight range.";
+        } else {
+            return "You are overweight. You should see your doctor.";
+        }
+    }
+
+    public double calculateBMI(double weight, double height){
+        return (weight / (height * height)) * 703;
+    }
+
+    public int promptInt(String prompt){
+        System.out.printf("What is your %s? ", prompt);
+        while(!in.hasNextInt()){
+            System.out.println("Please enter a number. ");
+            in.nextLine();
+        }
+        return Integer.parseInt(in.nextLine());
     }
 }
